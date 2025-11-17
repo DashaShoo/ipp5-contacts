ee977aae32abe5ea18c222e951ca28ba1d551478
DashaShoo <daryashubabko@gmail.com>
2025-11-17 18:32:11 +0300

    fix

--- server/server.js
+++ server/server.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on port " + PORT));
+
+
 // MongoDB connect
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB connected"))
